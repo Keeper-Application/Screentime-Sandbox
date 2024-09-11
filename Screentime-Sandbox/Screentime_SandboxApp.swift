ed1cb465993510e5b1af32cbc2f0b5c5fe547259
AyubsWrld <ayubfmohammed@gmail.com>
2024-09-11 11:08:15 -0600

    Initial commit

---
+++ Screentime-Sandbox/Screentime_SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screentime_SandboxApp.swift
+//  Screentime-Sandbox
+//
+//  Created by Ayub Mohamed on 2024-09-11.
+//
+
+import SwiftUI
+
+@main
+struct Screentime_SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Screentime-SandboxUITests/Screentime_SandboxUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Screentime_SandboxUITestsLaunchTests.swift
+//  Screentime-SandboxUITests
+//
+//  Created by Ayub Mohamed on 2024-09-11.
+//
+
+import XCTest
+
+final class Screentime_SandboxUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
